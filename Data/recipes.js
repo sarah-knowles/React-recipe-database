7cbcffc1f8d03eb907b6fa06bfb262b97ea40e73
sarah-knowles <sjk339@gmail.com>
2020-11-15 14:50:21 +1300

    new data added

--- Data/recipes.js
+++ Data/recipes.js
@@ -1,42 +1,46 @@
 const recipes = {
   meat: [
     {
-    id:101,
+    id:10101,
+    title: 'Best Meatloaf',
+    subtitle: 'Just as nice for lunch the next day',
+    image: 'images/meatloaf2.jpeg',
+    ingredients: "2 tbsp Olive Oil, 2 Onions, finely chopped, 3 Garlic Cloves, finely chopped, 1/2 tsp Freshly Ground Black Pepper, 2 tbsp Parsley, chopped, 350g Rindless, Streaky Bacon, 6 Sundried Tomatoes, thinly sliced 2 Eggs, beaten, 1kg Prime Beef Mince, Red Wine, 1/2 tsp Salt",
+    instructions: "Preheat the oven to 200øC. Heat the oil in a frying pan over moderate heat and fry the onions and garlic for about 10 minutes until the onion is soft and golden. Remove from the heat and cool. Put the onion mixture and the remaining ingredients, except the bacon into a bowl and mix well. Line a loaf tin with the bacon and put the mince mixture evenly into the tin. Top with bacon, cover with tinfoil and bake 1 hour or until the juices run clear when a skewer is inserted into the middle of the loaf. Remove from the oven and serve sliced, hot or cold."
+    },
+    {
+    id: 10102,
+    title: 'Lamb Cutlets',
+    subtitle: 'Great on the BBQ too',
+    image: '/images/Spiced-Lamb-Cutlets.jpg',
+    ingredients: "2 tsp ground cumin, 3 tsp paprika, 1 1/2 tsp ground coriander, 1/4 tsp cayenne pepper, 1/2 tsp ground turmeric, 5 garlic cloves, finely chopped, 100ml extra virgin olive oil, 1/4 cup (60ml) lemon juice, plus extra juice and wedges to serve, 12-18 French-trimmed lamb cutlets",
+    instructions: ''  
+  },
+    {
+    id: 10103,
+    title: 'Goulash',
+    subtitle: 'Great winter dinner',
+    image: 'server\public\images\Goulash.jpg',
+    ingredients: '⅓ cup vegetable oil, 3 onions, sliced, 2 tablespoons Hungarian sweet paprika, 2 teaspoons salt, ½ teaspoon ground black pepper, 3 pounds beef stew meat, cut into 1 1/2 inch cubes, 1 (6 ounce) can tomato paste, 1 ½ cups water, 1 clove garlic, minced, 1 teaspoon salt',
+    instructions: 'Heat oil in a large pot or Dutch oven over medium heat. Cook onions in oil until soft, stirring frequently. Remove onions and set aside. In a medium bowl, combine paprika, 2 teaspoons salt and pepper. Coat beef cubes in spice mixture, and cook in onion pot until brown on all sides. Return the onions to the pot, and pour in tomato paste, water, garlic and the remaining 1 teaspoon salt. Reduce heat to low, cover and simmer, stirring occasionally, 1 1/2 to 2 hours, or until meat is tender. Serve hot with sour cream.'
+    },
+    {
+    id: 10104,
+    title: ,
+    subtitle: ,
+    image: ,
+    ingredients: ,
+    instructions:
+    },
+    {
+    id: 10105,
     title: ,
     subtitle: ,
     image: ,
     instructions:
     },
     {
-    id: ,
-    title: ,
-    subtitle: ,
-    image: ,
-    instructions:
-    },
-    {
-    id: ,
-    title: ,
-    subtitle: ,
-    image: ,
-    instructions:
-    },
-    {
-    id: ,
-    title: ,
-    subtitle: ,
-    image: ,
-    instructions:
-    },
-    {
-    id: ,
-    title: ,
-    subtitle: ,
-    image: ,
-    instructions:
-    },
-    {
-    id: ,
+    id: 10106,
     title: ,
     subtitle: ,
     image: ,
@@ -45,42 +49,43 @@ const recipes = {
   ],
   pasta: [
     {
-    id: ,
+    id: 10201,
+    title: 'Fetticine Carbonara',
+    subtitle: 'Easy summer diner',
+    image: '/images/fetticine.jpg',
+    ingredients: '1 tablespoon olive oil, 1 yellow onion, chopped, 2 garlic cloves, crushed, 4 slices bacon, fat removed and chopped into chunks, 600 ml cream, 4 eggs, 1⁄2 cup parmesan cheese, 1 teaspoon salt, pepper, 500g fettuccine pasta, 1 teaspoon oil for boiling pasta, 1 teaspoon salt for boiling pasta, parmesan cheese',
+    instructions: 'Place water in a large saucepan with a little oil and salt. Bring to the boil and add fettucine. Stir for a few minutes to prevent fettucine from sticking together. Meanwhile, heat oil in a saucepan and gently cook onion, garlic and bacon on medium heat until onion becomes soft being careful not to burn. In a bowl beat the eggs. Pour in cream and whisk together. Add salt and pepper and mix well. Transfer cream mixture into saucepan (off the heat) with onion, garlic and bacon and stir quickly to combine. When pasta is cooked, drain into a large serving bowl and stir in the carbonara sauce and combine well. The heat of the pasta will cook the sauce and thicken. Serve immediately with some extra parmesan cheese.'
+    },
+    {
+    id: 10202,
     title: ,
     subtitle: ,
     image: ,
     instructions:
     },
     {
-    id: ,
+    id: 10203,
     title: ,
     subtitle: ,
     image: ,
     instructions:
     },
     {
-    id: ,
+    id: 10204,
     title: ,
     subtitle: ,
     image: ,
     instructions:
     },
     {
-    id: ,
+    id: 10205,
     title: ,
     subtitle: ,
     image: ,
     instructions:
     },
     {
-    id: ,
-    title: ,
-    subtitle: ,
-    image: ,
-    instructions:
-    },
-    {
-    id: ,
+    id: 10206,
     title: ,
     subtitle: ,
     image: ,
@@ -89,42 +94,42 @@ const recipes = {
   ],
   dessert: [
     {
-    id: ,
+    id: 10301,
+    title: 'Bailey\'s Cheesecake',
+    subtitle: 'Single serve rich dessert',
+    image: 'images/cheesecake2.jpg',
+    instructions: 
+    },
+    {
+    id: 10302,
     title: ,
     subtitle: ,
     image: ,
     instructions:
     },
     {
-    id: ,
+    id: 10303,
     title: ,
     subtitle: ,
     image: ,
     instructions:
     },
     {
-    id: ,
+    id: 10304,
     title: ,
     subtitle: ,
     image: ,
     instructions:
     },
     {
-    id: ,
+    id: 10305,
     title: ,
     subtitle: ,
     image: ,
     instructions:
     },
     {
-    id: ,
-    title: ,
-    subtitle: ,
-    image: ,
-    instructions:
-    },
-    {
-    id: ,
+    id: 10306,
     title: ,
      subtitle: ,
     image: ,
--- client/components/Recipe.jsx
+++ client/components/Recipe.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const Recipe = ({title, subheading, image}) => {
+const Recipe = ({title, subheading, image, ingredients, instructions}) => {
   return(
     <div>
       <h3>{title}</h3>
       <p>{subheading}</p>
       <img src={image} alt=""/>
+      <p>{ingredients}</p>
       <p>{instructions}</p>
     </div>
   )
